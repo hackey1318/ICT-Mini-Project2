e3a586a2bd92bda3c6da2530e5fceff4b4211c81
yesim <hackey1318@naver.com>
2025-03-06 14:00:02 +0900

    Server & Front SetUp

---
+++ server/eventHomePage/src/main/java/com/ict/eventHomePage/EventHomePageApplication.java
@@ -0,0 +1,13 @@
+package com.ict.eventHomePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHomePageApplication.class, args);
+	}
+
+}
---
+++ server/eventHomePage/src/test/java/com/ict/eventHomePage/EventHomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.eventHomePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

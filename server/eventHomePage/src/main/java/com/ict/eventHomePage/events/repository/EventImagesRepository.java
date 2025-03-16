251c54084352491e25b138e64159c75f61f0abfb
ajh0714 <ajh970714@gmail.com>
2025-03-16 15:02:57 +0900

    ㅇㅇ

--- server/eventHomePage/src/main/java/com/ict/eventHomePage/events/repository/EventImagesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ict.eventHomePage.events.repository;
-
-import com.ict.eventHomePage.domain.EventImages;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventImagesRepository extends JpaRepository<EventImages, Long> {
-}

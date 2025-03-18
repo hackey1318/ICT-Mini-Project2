c4da42546e794624fec2866b5e19abe38fa55fc3
ks09-d <qjeltk52@naver.com>
2025-03-18 16:26:10 +0900

    Develop 최신버전 review로 pull

--- front/event-home-page/src/eventpages/EventView.js
+++ front/event-home-page/src/eventpages/EventView.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import moment from 'moment';
 import '../eventCss/EventView.css';
 import '../css/replyList.css';
 import ReviewEdit from '../js/event/ReviewEdit'
 import ReviewDelete from '../js/event/ReviewDelete';
-import ReplyModal from './ReplyModal';
-import styled from 'styled-components';
 import axios from 'axios';
 import addFile from '../img/plus.jpg';
+import '../css/replyModal.css';
+
 
 function EventView() {
   const location = useLocation();
@@ -19,6 +19,7 @@ function EventView() {
   let [content, setContent] = useState('');
   let [isModalOpen, setIsModalOpen] = useState(false);
   const runfile = useRef([]);  //type==file실행 준비 및 사진 갯수제한용
+  let [replies, setReplies] = useState([]);
 
   useEffect(() => {
     if (event && mapRef.current) {
@@ -50,6 +51,16 @@ function EventView() {
     return <div>이벤트 정보를 찾을 수 없습니다.</div>;
   }
 
+  //후기 리스트 출력
+  /*useEffect(() => {
+    axios.get('http://localhost:9988/reply/getReplies')
+    .then((response) => {
+        console.log(response.data);
+        setReplies(response.data);
+    })
+    .catch((error) => {console.log(error)});
+  }, [response.data]);*/
+
   //모달창 함수
   //후기 제목 함수
   function setTitleValue(event) {
@@ -57,71 +68,90 @@ function EventView() {
     console.log(title);
 }
 
-//후기 내용 함수
-function setContentValue(event) {
-    setContent(event.target.value);
-    console.log(content);
-}
+  //후기 내용 함수
+  function setContentValue(event) {
+      setContent(event.target.value);
+      console.log(content);
+  }
 
-function addReply(event) {
-    event.preventDefault();
+  function addReply(event) {
+      event.preventDefault();   
 
-    axios.post("http://localhost:9988/reply/addReply")
-    .then(function(response) {
-        console.log(response.data);
-
-        if(response.data.content=="") {
+      axios.post("http://localhost:9988/reply/addReply")
+      .then(function(response) {
+          console.log(response.data);
+          if(response.data.content=="") {
             alert("내용을 입력해주세요.")
-        }             
-    })
-    .catch(function(error) {
-        console.log(error);
-    })
+            return false;
+          }  
+
+          let replyData = {
+            title: title,
+            content: content
+          }
+
+          setTitle('');
+          setContent('');
+          setIsModalOpen(false);       
+      })
+      .catch(function(error) {console.log(error)})
+  }
+
+  function runInputFile() {  //type=file 실행
+      if(runfile.current) {
+          runfile.current.click();  //file 선택창 열기
+      }
+
+      if(!runfile.current.hasChangeListener) {
+          runfile.current.addEventListener('change', (event) => {
+              const files = event.target.files;
+              const imgList = document.querySelector('.imgList');
+              const maxImage = 3;
+
+              const existingImages = imgList.querySelectorAll('div').length;
+              for(let i=0; i<files.length; i++) {
+                  if(existingImages + i >= maxImage) {
+                      alert('이미지는 3개까지 첨부해주세요.');
+                      event.target.value = "";
+                      break;
+                  }
+
+                  const reader = new FileReader();
+                  reader.onload = function(e) {   //div에 이미지 추가
+                      const div = document.createElement('div');
+                      div.style.backgroundImage = `url(${e.target.result})`;
+                      div.style.cursor = 'pointer';
+                      div.addEventListener('click', delImg);
+
+                      imgList.appendChild(div);  
+                  };
+                  reader.readAsDataURL(files[i]);
+              }
+          })
+          runfile.current.hasChangeListener = true;
+      }
+  }
+
+  function opacityController() {
+      const div = document.getElementById("plus-container");
+      div.style.opacity = 1;
+      div.style.transition = 'all, 500ms';
+  }
+
+  function opacityController2() {
+    const div = document.getElementById("plus-container");
+    div.style.opacity = 0.3;
+    div.style.transition = 'all, 500ms';
 }
 
-function runInputFile() {  //type=file 실행
-    if(runfile.current) {
-        runfile.current.click();  //file 선택창 열기
-    }
+  function delImg(event) {  //올리는 이미지 클릭시 제거
+      const imgDiv = event.target;
+      const imgList = document.querySelector('.imgList');
 
-    if(!runfile.current.hasChangeListener) {
-        runfile.current.addEventListener('change', (event) => {
-            const files = event.target.files;
-            const imgList = document.querySelector('.imgList');
-            const maxImage = 3;
-
-            const existingImages = imgList.querySelectorAll('div').length;
-            for(let i=0; i<files.length; i++) {
-                if(existingImages + i >= maxImage) {
-                    alert('이미지는 3개까지 첨부해주세요.');
-                    event.target.value = "";
-                    break;
-                }
-
-                const reader = new FileReader();
-                reader.onload = function(e) {   //div에 이미지 추가
-                    const div = document.createElement('div');
-                    div.style.backgroundImage = `url(${e.target.result})`;
-                    div.style.cursor = 'pointer';
-                    div.addEventListener('click', delImg);
-
-                    imgList.appendChild(div);  
-                };
-                reader.readAsDataURL(files[i]);
-            }
-        })
-        runfile.current.hasChangeListener = true;
-    }
-}
-
-function delImg(event) {  //올리는 이미지 클릭시 제거
-    const imgDiv = event.target;
-    const imgList = document.querySelector('.imgList');
-
-    if(imgList.contains(imgDiv)) {
-        imgList.removeChild(imgDiv);
-    }
-}
+      if(imgList.contains(imgDiv)) {
+          imgList.removeChild(imgDiv);
+      }
+  }
 
   return (
     <div className="event-view-container">
@@ -160,10 +190,9 @@ function delImg(event) {  //올리는 이미지 클릭시 제거
                   {
                       //const list = map() => 
                       <div className='replyList'>
-                          <ul>
-                                <li id='username'></li>
-                                <li id='title'>제목</li>
-
+                            <ul>
+                                <li id='username'>{replies.username}</li>
+                                <li id='title'>{replies.title}</li>
                                 {
                                     <div>
                                         <label className='editor' onClick={ReviewEdit} style={{marginRight: '20px'}}>수정</label>
@@ -179,29 +208,32 @@ function delImg(event) {  //올리는 이미지 클릭시 제거
                   <input type='button' id='openWriteForm' value='후기쓰기' onClick={() => setIsModalOpen(true)}/>
               </div>
 
-              <div id="modalSpace">
-                  {isModalOpen && (
-                      <div className='modalContainer' style={{width: '100%', margin: "10% 10% 0"}}>            
-                          <div className='writeForm'>
-                              <form onSubmit={addReply}>
-                                  <input type='text' placeholder='제목을 입력해주세요.' name='title' value={title} onChange={setTitleValue}/><br/>
-                                  <textarea type='text' className='class="form-control"' placeholder='후기를 입력해주세요.' name='content'  
-                                                        value={content} onChange={setContentValue}/><br/>
-                          
-                                  <label style={{fontSize: '0.7em', position: 'relative', left: '10px', top: '-5px'}}>사진첨부(최대 3장)</label><br/>
-                                  <input type='file' multiple ref={runfile}
-                                        style={{position: 'relative', left: '10px', top: '-5px', opacity: '0', width: '65%' }}/>
-                                  <img src={addFile} id='addFile' style={{cursor: 'pointer'}} onClick={runInputFile}/>
-              
-                                  <div className='imgList'></div>
-              
-                                  <input type='submit' value='등 록'/>
-                                  <input type='button' value='취 소' onClick={() => setIsModalOpen(false)}/>
-                              </form>
-                          </div>
+              {isModalOpen && (                  
+                  <div className='modalContainer' style={{width: '100%', margin: "10% 10% 0"}}>            
+                      <div className='writeForm' >
+                          <form onSubmit={addReply}>
+                              <input type='text' className='write-space' placeholder='제목을 입력해주세요.' name='title' value={title} onChange={setTitleValue}/><br/>
+                              <textarea type='text' className='festival-modal-textarea' placeholder="후기내용을 입력하세요" value={content} onChange={setContentValue}/>
+                      
+                              <label style={{fontSize: '0.7em', position: 'relative', left: '20px', top: '15px'}}>사진첨부(최대 3장)</label><br/>
+                              <input type='file' multiple ref={runfile}
+                                    style={{position: 'relative', left: '10px', top: '-5px', opacity: '0', width: '65%' }}/>
+
+                              <div id="plus-container" onMouseOver={opacityController} 
+                                                        onMouseOut={opacityController2}></div>
+                              <img src={addFile} id='addFile' style={{cursor: 'pointer'}} 
+                                                        onMouseOver={opacityController} 
+                                                        onMouseOut={opacityController2} 
+                                                        onClick={runInputFile}/>
+
+                              <div className='imgList'></div>
+          
+                              <input type='submit' value='등 록'/>
+                              <input type='button' value='취 소' onClick={() => setIsModalOpen(false)}/>
+                          </form>
                       </div>
-                  )}
-              </div>
+                  </div>
+                )}
           </div>
       </div>            
   </div>
--- server/eventHomePage/src/main/java/com/ict/eventHomePage/reply/controller/ReplyController.java
+++ server/eventHomePage/src/main/java/com/ict/eventHomePage/reply/controller/ReplyController.java
@@ -1,12 +1,16 @@
 package com.ict.eventHomePage.reply.controller;
 
 import com.ict.eventHomePage.domain.Replies;
-import com.ict.eventHomePage.reply.controller.request.ReplyRequest;
-import com.ict.eventHomePage.reply.service.ReplyService;
+import com.ict.eventHomePage.domain.ReplyImages;
+import com.ict.eventHomePage.fileService.controller.request.ImageRequest;
+import com.ict.eventHomePage.reply.controller.request.UserRequest;
 import com.ict.eventHomePage.reply.service.impl.ReplyServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +20,50 @@ import java.util.List;
 public class ReplyController {
 
     private final ReplyServiceImpl replyService;
+    private static String FILE_PATH = null;
 
     @PostMapping("/addReply")
-    public List<Replies> addReply(@RequestBody ReplyRequest request) {
+    public List<Replies> addReply(@RequestBody Replies replies, UserRequest userRequest, MultipartFile[] files, HttpServletRequest request) {
 
-        return replyService.addReply(request.getUserNo());
+        FILE_PATH = request.getServletContext().getRealPath("/uploads");
+        List<File> fileList = new ArrayList<File>();
+
+        try {
+            Replies resultReplies = replyService.dataInsert(replies);
+
+            for(MultipartFile mf : files) {
+                String orgFileName = mf.getOriginalFilename();
+                File f = new File(FILE_PATH, orgFileName);
+                int point = orgFileName.lastIndexOf(".");
+                String fName = orgFileName.substring(0, point);
+                String eName = orgFileName.substring(point+1);
+
+                if(f.exists()) {
+                    for(int i=1; ; i++) {
+                        String newFileName = fName + "(" + i + ")." + eName;
+
+                        File newFile = new File(FILE_PATH, newFileName);
+                        if(!newFile.exists()) {
+                            fName = fName + "(" + i + ").";
+                            break;
+                        }
+                    }
+                }
+
+                //ReplyImages replyImages = new ReplyImages(0, 0, );
+            }
+        } catch(Exception e) {
+
+        }
+
+
+        return replyService.addReply(userRequest.getUserNo());
+    }
+
+    @GetMapping("/getReplies")
+    public List<Replies> getReplies(String userId, String content) {
+        List<Replies> replies = replyService.getReplies(userId, content);
+        return replies;
     }
 }
 

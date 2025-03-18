8979ea1e19b39bfc269c4e8f6d5f814fec75e002
ks09-d <qjeltk52@naver.com>
2025-03-18 16:54:08 +0900

    사용자 정보 로직 추가

--- server/eventHomePage/src/main/java/com/ict/eventHomePage/reply/controller/ReplyController.java
+++ server/eventHomePage/src/main/java/com/ict/eventHomePage/reply/controller/ReplyController.java
@@ -1,10 +1,10 @@
 package com.ict.eventHomePage.reply.controller;
 
+import com.ict.eventHomePage.common.config.AuthCheck;
 import com.ict.eventHomePage.domain.Replies;
-import com.ict.eventHomePage.domain.ReplyImages;
-import com.ict.eventHomePage.fileService.controller.request.ImageRequest;
-import com.ict.eventHomePage.reply.controller.request.UserRequest;
+//import com.ict.eventHomePage.reply.controller.request.UserRequest;
 import com.ict.eventHomePage.reply.service.impl.ReplyServiceImpl;
+import com.ict.eventHomePage.users.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,36 +14,41 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.ict.eventHomePage.domain.constant.UserRole.ADMIN;
+import static com.ict.eventHomePage.domain.constant.UserRole.USER;
+
 @RestController
 @RequestMapping("/reply")
 @RequiredArgsConstructor
 public class ReplyController {
 
     private final ReplyServiceImpl replyService;
+    private final AuthService authService;
     private static String FILE_PATH = null;
 
     @PostMapping("/addReply")
-    public List<Replies> addReply(@RequestBody Replies replies, UserRequest userRequest, MultipartFile[] files, HttpServletRequest request) {
+    public List<Replies> addReply(@RequestBody Replies replies, MultipartFile[] files, HttpServletRequest request) {
 
+        String userId = AuthCheck.getUserId(USER, ADMIN);
         FILE_PATH = request.getServletContext().getRealPath("/uploads");
         List<File> fileList = new ArrayList<File>();
 
         try {
             Replies resultReplies = replyService.dataInsert(replies);
 
-            for(MultipartFile mf : files) {
+            for (MultipartFile mf : files) {
                 String orgFileName = mf.getOriginalFilename();
                 File f = new File(FILE_PATH, orgFileName);
                 int point = orgFileName.lastIndexOf(".");
                 String fName = orgFileName.substring(0, point);
-                String eName = orgFileName.substring(point+1);
+                String eName = orgFileName.substring(point + 1);
 
-                if(f.exists()) {
-                    for(int i=1; ; i++) {
+                if (f.exists()) {
+                    for (int i = 1; ; i++) {
                         String newFileName = fName + "(" + i + ")." + eName;
 
                         File newFile = new File(FILE_PATH, newFileName);
-                        if(!newFile.exists()) {
+                        if (!newFile.exists()) {
                             fName = fName + "(" + i + ").";
                             break;
                         }
@@ -52,12 +57,12 @@ public class ReplyController {
 
                 //ReplyImages replyImages = new ReplyImages(0, 0, );
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
 
         }
 
 
-        return replyService.addReply(userRequest.getUserNo());
+        return replyService.addReply(authService.getUser(userId).getNo());
     }
 
     @GetMapping("/getReplies")
